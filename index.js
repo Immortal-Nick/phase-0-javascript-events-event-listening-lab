6f90f2381537fb491502b04f261bf577f6dd1757
Nixon <kiplangatnixon28@gmail.com>
2024-07-15 18:00:14 +0300

    adds eventlistener

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+  if (button) {
+    button.addEventListener("click", () => {
+      console.log("Button was clicked!");
+      
+    });
+  } else {
+    console.error("Button element not found.");
+  }
 }
